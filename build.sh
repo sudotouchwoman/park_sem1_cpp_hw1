216ffc4beaf930ab832a7b7bc13843ea148b196b
Nikita Teterin <nekitonum@gmail.com>
2021-10-14 00:07:17 +0300

    fixed bad interpreter

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 echo "Setting up the project"
 TESTDIR=googletest/
 [[ -d $TESTDIR ]] && echo "google test framework seems to be installed already" || git clone https://github.com/google/googletest.git
